8036e4c1c2413f498baee94103bb10729ec75905
Nicolas Sauvage <noki.sauvage@gmail.com>
2022-05-16 23:16:42 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<div class="container">
+	<div class="min-h-screen bg-[url('/us.jpg')] bg-no-repeat bg-cover bg-center max-w-full">
+		<div class="flex flex-col justify-around items-stretch min-h-full flex-grow text-white">
+			<div>Mathilde et Nicolas</div>
+			<div>
+				<div>24 septembre 2022</div>
+			</div>
+            <div class="flex flex-grow flex-col mb-72" />
+			<div class="flex-1 flex flex-grow justify-center items-center">
+                <span class="white-line" />
+				<span class=" text-4xl ml-4 mr-4 font-serif text-center"> 
+                    24 septembre 2022
+                </span>
+				<span class="white-line" />
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+    .white-line {
+        @apply border-b-white border-b-2 w-1/5 h-3;
+    }
+</style>
